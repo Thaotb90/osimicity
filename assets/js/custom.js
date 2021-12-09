5fbc9d9ee66a6aab7b6e0fb8d869f3589d459462
Kea Mr <ptkien.code@gmail.com>
2021-12-09 15:14:37 +0700

    up

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -161,3 +161,9 @@ $('.team-slider').slick({
     delay: 0, // values from 0 to 3000, with step 50ms
     duration: 1000 // values from 0 to 3000, with step 50ms
   });
+
+
+  window.onload = function(){
+    //hide the preloader
+    document.querySelector("#preloader").style.display = "none";
+}
